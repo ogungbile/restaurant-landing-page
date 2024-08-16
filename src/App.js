d5311343460ce33d956568d20640ece4bfe5343a
ogungbile <oluwatobi.ogungbile@yahoo.com>
2024-08-16 22:06:00 +0100

    the contact and footer are added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Home from './Components/Home';
 import About from "./Components/About";
 import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Home/>
       <About/>
       <Work/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
